8c2d9453ec41e7a646c71d722aaf666764a41b62
John Doe <johndoe@example.com>
2024-03-19 18:33:31 +0530

    1-4 Programs

---
+++ Basics.py
@@ -0,0 +1,17 @@
+#Exercise 1
+print("Beny Jermila.S")
+print("Ex-1: Mailing address\n 640,Anna Nagar,\nTindivanam-604001")
+#Ex:2
+Name=input("Enter your name:")
+print("Ex-2:Hello\n Hello!",Name)
+#Ex:3
+length=float(input("Length of the room: "))
+width=float(input("Width of the room: "))
+Area=length*width
+print("Ex-3: Area of room\n Area of the room=" ,Area,"Sq.m")
+#Exercise 4
+length=float(input("length of the field:"))
+width=float(input("width of the field:"))
+area_sq_ft=length*width
+area_acre=area_sq_ft/43560
+print("area of the field in acres:",area_acre)
